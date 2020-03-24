d6f89addd976974bd0bef946c27a9c72c8599bfc
Manikandan A <manikandan.a@cardinality.ai>
2020-03-24 17:56:37 +0530

    ok

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const fs = require('fs');
-let PDFExportOptions = JSON.parse(fs.readFileSync('./PDF-Export-Options.json', 'utf-8'))
+const PDFExportOptionsBrochure = JSON.parse(fs.readFileSync('./PDF-Export-Options-Brochure.json', 'utf-8'));
+const PDFExportOptionsTable = JSON.parse(fs.readFileSync('./PDF-Export-Options-Table.json', 'utf-8'));
 const pdftemplate = require('./pdftemplate');
 const newpdftemplate = require('./newpdftemplate');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    // console.log(JSON.stringify(PDFExportOptions, null, 2));
-    res.send(pdftemplate.pdfhtmlBrochure(PDFExportOptions));
+    res.send(pdftemplate.pdfhtmlBrochure(PDFExportOptionsBrochure));
 });
 
-app.get('/catelog', (req, res) => {
-    // console.log(JSON.stringify(PDFExportOptions, null, 2));
-    res.send(newpdftemplate.pdfhtmlTable(PDFExportOptions));
+app.get('/table', (req, res) => {
+    res.send(newpdftemplate.pdfhtmlTable(PDFExportOptionsTable));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
